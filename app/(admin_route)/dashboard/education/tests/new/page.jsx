9a495c6ad1f1df2d30ac56583fdb7441d1fbcb34
Valentin Zagrebin <valentin.zagrebin.93@gmail.com>
2023-09-26 01:05:09 +0300

    add QuestionList to NewTestPage, add OkButton to QuestModal

--- app/(admin_route)/dashboard/education/tests/new/page.jsx
+++ app/(admin_route)/dashboard/education/tests/new/page.jsx
@@ -1,5 +1,7 @@
 'use client';
 import NewQuestModal from "@components/NewQuestModal";
+import DeleteIcon from "@components/icons/DeleteIcon";
+import EditIcon from "@components/icons/EditIcon";
 import { useState } from "react";
 
 const NewTestPage = () => {
@@ -17,12 +19,16 @@ const NewTestPage = () => {
     setNewQuestModal(false);
   }
 
+  const handleDeleteQuest = (id) => {
+    setQuestion([...question.filter((q) => q.q_id !== id)]);
+  }
+
   return (
     <div className="font-rubik text-neutral-700 dark:text-white px-5 md:px-20">
       <div className="mb-10">
         <h1 className="text-lg md:text-4xl text-neutral-700 dark:text-white py-5">Создание нового теста</h1>
       </div>
-      <div className="flex gap-60">
+      <div className="flex justify-between gap-20">
         <div className="min-w-[400px] flex flex-col gap-5">
           <div className="flex gap-3 justify-between items-center">
             <p>Название теста</p>
@@ -80,8 +86,8 @@ const NewTestPage = () => {
             />
           </div>
         </div>
-        <div>
-          <div className="flex items-center gap-10">
+        <div className="w-full">
+          <div className="flex justify-between items-center gap-10 mb-5">
             <h1 className="text-2xl">Вопросы</h1>
             <button
               className="bg-violet-500 text-white px-3 py-2 rounded-md hover:bg-violet-600 shadow-md"
@@ -90,9 +96,36 @@ const NewTestPage = () => {
             >
               Добавить</button>
           </div>
+          <ul className="flex flex-col gap-3">
+          {question?.sort((a,b) => a.q_queue - b.q_queue).map((quest) =>
+            <li
+              className="flex justify-between items-center bg-white dark:bg-neutral-800 rounded-md shadow-md p-5"
+              key={quest.q_id}
+            >
+              <div className="flex gap-5">
+                <p>{quest.q_queue}.</p>
+                <p>{quest.quest}</p>
+              </div>
+              <div className="flex gap-3 items-center">
+                <button
+                  className="bg-neutral-100 hover:bg-neutral-200 dark:bg-neutral-900 dark:hover:bg-neutral-700 p-2 rounded-md shadow-md"
+                  type="button"
+                >
+                  <EditIcon className='fill-neutral-700 dark:fill-white' width='24px' height='24px' />
+                </button>
+                <button
+                  className="bg-red-500 hover:bg-red-600 p-2 rounded-md shadow-md"
+                  type="button"
+                  onClick={() => handleDeleteQuest(quest.q_id)}
+                >
+                  <DeleteIcon className='fill-white' width='24px' height='24px' />
+                </button>
+              </div>
+            </li>)}
+          </ul>
         </div>
       </div>
-      {newQuestModal && <NewQuestModal closeModal={closeModal} />}
+      {newQuestModal && <NewQuestModal closeModal={closeModal} question={question} setQuestion={setQuestion} />}
     </div>
   )
 }
--- components/NewQuestModal.jsx
+++ components/NewQuestModal.jsx
@@ -6,9 +6,9 @@ import CloseIcon from './icons/CloseIcon';
 import AddIcon from './icons/AddIcon';
 import DeleteIcon from './icons/DeleteIcon';
 
-const NewQuestModal = ({ closeModal }) => {
+const NewQuestModal = ({ closeModal, question, setQuestion }) => {
 
-    const [newQuest, setNewQuest] = useState({quest: '', type: '', answers: []});
+    const [newQuest, setNewQuest] = useState({quest: '', q_queue: question.length + 1, type: '', answers: []});
 
 
     const handleAddAnswer = () => {
@@ -19,9 +19,14 @@ const NewQuestModal = ({ closeModal }) => {
       setNewQuest({...newQuest, answers: [...newQuest.answers.filter((a) => a.a_id !== id)]});
     }
 
+    const handleAddQuest = () => {
+      setQuestion([...question, {...newQuest, q_id: nanoid()}]);
+      closeModal();
+    }
+
   return (
     <div className='fixed top-0 bottom-0 left-0 right-0 black_opacity flex justify-center items-center px-0 sm:px-5 z-50' onClick={closeModal}>
-      <div className='w-[1000px] bg-white dark:bg-neutral-800 sm:rounded-2xl opacity-100 p-5' onClick={(e) => e.stopPropagation()}>
+      <div className='w-[1000px] bg-neutral-100 dark:bg-neutral-800 sm:rounded-2xl opacity-100 p-5' onClick={(e) => e.stopPropagation()}>
         <div className='flex justify-between items-center mb-5'>
           <h1 className='text-2xl'>Добавление нового вопроса</h1>
           <div onClick={closeModal}>
@@ -32,10 +37,19 @@ const NewQuestModal = ({ closeModal }) => {
             />
           </div>
         </div>
-        <div className='flex items-start gap-20'>
-          <div className='w-full'>
-            <div className='flex flex-col gap-3'>
-              <p>Тип вопроса:</p>
+        <div className='flex gap-20'>
+          <div className='w-full flex flex-col gap-5'>
+            <div className='flex justify-between items-center gap-3'>
+              <h1 className='w-full'>Номер вопроса:</h1>
+              <input
+                className='block w-[60px] text-neutral-700 dark:text-white bg-white dark:bg-neutral-900 rounded-md shadow-md p-2'
+                type='number'
+                value={newQuest.q_queue}
+                onChange={(e) => setNewQuest({...newQuest, q_queue: e.target.value})}
+              />
+            </div>
+            <div className='w-full flex justify-between items-center gap-3'>
+              <h1 className='w-full'>Тип вопроса:</h1>
               <NewFormSelect
                 data={['Единственный выбор', 'Множественный выбор']}
                 value={newQuest}
@@ -43,10 +57,10 @@ const NewQuestModal = ({ closeModal }) => {
                 exist={'type'}
               />
             </div>
-            <div className='flex flex-col gap-3 mt-10'>
-              <h1>Текст вопроса:</h1>
+            <div className='flex flex-col gap-3'>
+              <h1 className='py-2'>Текст вопроса:</h1>
               <textarea
-                className='w-full resize-none outline-none rounded-md p-2'
+                className='w-full bg-white dark:bg-neutral-900 text-neutral-700 dark:text-white resize-none outline-none rounded-md p-2 shadow-md'
                 rows={5}
                 cols={30}
                 value={newQuest.quest}
@@ -54,52 +68,63 @@ const NewQuestModal = ({ closeModal }) => {
               />
             </div>
           </div>
-          <div className='w-full'>
-            <div className='flex flex-col gap-3'>
-              <div className='flex justify-between items-center'>
-                <h1>Варианты ответов:</h1>
+          <div className='w-full h-auto'>
+            <div className='h-full flex flex-col justify-between gap-3'>
+              <div className='flex flex-col gap-3'>
+                <div className='flex justify-between items-center'>
+                  <h1>Варианты ответов:</h1>
+                  <button
+                    className='text-white bg-violet-500 hover:bg-violet-600 px-3 py-2 rounded-md shadow-xl'
+                    type='button'
+                    onClick={handleAddAnswer}
+                  >
+                    Добавить
+                  </button>
+                </div>
+                <ul className='flex flex-col gap-3'>
+                  {newQuest.answers?.sort((a, b) => a.queue - b.queue).map((answer) =>
+                  <li
+                    className='flex items-center gap-5'
+                    key={answer.a_id}
+                  >
+                    <div>
+                      <input
+                        type='radio'
+                        id={`radio-${answer.a_id}`}
+                        name='answer-group'
+                        value={answer.a_id}
+                        onChange={(e) => setNewQuest({...newQuest, answers: [...newQuest.answers.filter((a) => a.a_id !== answer.a_id), { ...newQuest.answers.filter((a) => a.a_id === answer.a_id)[0], value: e.target.value}]})}
+                      />
+                      <label htmlFor={`radio-${answer.a_id}`}></label>
+                    </div>
+                    <div className='w-full'>
+                      <input
+                        className='w-full bg-white dark:bg-neutral-900 rounded-md shadow-md outline-none px-3 py-2'
+                        type='text'
+                        value={newQuest.answers.filter((a) => a.a_id === answer.a_id)[0].text}
+                        onChange={(e) => setNewQuest({...newQuest, answers: [...newQuest.answers.filter((a) => a.a_id !== answer.a_id), { ...newQuest.answers.filter((a) => a.a_id === answer.a_id)[0], text: e.target.value }]})}
+                      />
+                    </div>
+                    <div>
+                      <button
+                        className='block bg-red-500 hover:bg-red-600 p-1 rounded-md shadow-xl'
+                        onClick={() => handleDeleteAnswer(answer.a_id)}
+                      >
+                        <DeleteIcon className='fill-white' width='24px' height='24px' />
+                      </button>
+                    </div>
+                  </li>)}
+                </ul>
+              </div>
+              <div className='flex justify-end'>
                 <button
-                  className='bg-violet-500 hover:bg-violet-600 px-3 py-2 rounded-md'
+                  className='text-white bg-violet-500 hover:bg-violet-600 px-3 py-2 rounded-md shadow-xl'
                   type='button'
-                  onClick={handleAddAnswer}
+                  onClick={handleAddQuest}
                 >
-                  Добавить
+                  Ок
                 </button>
               </div>
-              <ul className='flex flex-col gap-3'>
-                {newQuest.answers?.sort((a, b) => a.queue - b.queue).map((answer) =>
-                <li
-                  className='flex items-center gap-5'
-                  key={answer.a_id}
-                >
-                  <div>
-                    <input
-                      type='radio'
-                      id={`radio-${answer.a_id}`}
-                      name='answer-group'
-                      value={answer.a_id}
-                      onChange={(e) => setNewQuest({...newQuest, answers: [...newQuest.answers.filter((a) => a.a_id !== answer.a_id), { ...newQuest.answers.filter((a) => a.a_id === answer.a_id)[0], value: e.target.value}]})}
-                    />
-                    <label htmlFor={`radio-${answer.a_id}`}></label>
-                  </div>
-                  <div className='w-full'>
-                    <input
-                      className='w-full rounded-md outline-none px-3 py-2'
-                      type='text'
-                      value={newQuest.answers.filter((a) => a.a_id === answer.a_id)[0].text}
-                      onChange={(e) => setNewQuest({...newQuest, answers: [...newQuest.answers.filter((a) => a.a_id !== answer.a_id), { ...newQuest.answers.filter((a) => a.a_id === answer.a_id)[0], text: e.target.value }]})}
-                    />
-                  </div>
-                  <div>
-                    <button
-                      className='block bg-red-500 hover:bg-red-600 p-1 rounded-md'
-                      onClick={() => handleDeleteAnswer(answer.a_id)}
-                    >
-                      <DeleteIcon className='fill-white' width='24px' height='24px' />
-                    </button>
-                  </div>
-                </li>)}
-              </ul>
             </div>
           </div>
         </div>
